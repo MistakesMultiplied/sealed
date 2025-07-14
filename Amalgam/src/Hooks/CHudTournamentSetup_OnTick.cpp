725d5f1141a5c228182ba8240f217275d721bd9c
mlemlody <garciaisheree@gmail.com>
2025-07-14 18:56:59 +0200

    Update CHudTournamentSetup_OnTick.cpp

--- Amalgam/src/Hooks/CHudTournamentSetup_OnTick.cpp
+++ Amalgam/src/Hooks/CHudTournamentSetup_OnTick.cpp
@@ -1,7 +1,7 @@
 #include "../SDK/SDK.h"
 #include "../Features/Misc/Misc.h"
 
-MAKE_SIGNATURE(CHudTournamentSetup_OnTick, "client.dll", "48 83 EC ? E8 ? ? ? ? 48 85 C0 74 ? 48 8B C8 E8 ? ? ? ? 48 83 C4 ? C3", 0x0);
+MAKE_SIGNATURE(CHudTournamentSetup_OnTick, "client.dll", "40 57 48 83 EC ? 48 8B F9 E8 ? ? ? ? 48 85 C0 0F 84 ? ? ? ? 48 89 5C 24", 0x0);
 
 MAKE_HOOK(CHudTournamentSetup_OnTick, S::CHudTournamentSetup_OnTick(), void,
 	void* rcx)
